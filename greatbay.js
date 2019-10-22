969f1e6363a0cdea9c648115b4db02ad8e858c83
Amjad Shaaban <theamjad@outlook.com>
2019-10-21 21:12:56 -0400

    Inq done

--- greatbay.js
+++ greatbay.js
@@ -15,7 +15,40 @@ var connection = mysql.createConnection({
   database: "greatbay_db"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-  });
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+});
+
+
+var mainMeun = () => {
+  inquirer.prompt([{
+      type: "input",
+      message: "Enter User name.",
+      namw: "userName"
+    },
+    {
+      type: "password",
+      message: "Enter user password.",
+      mask: "8",
+      name: "password"
+    },
+    {
+      type: "list",
+      message: "Select an option",
+      choices: [
+        "Place Bid",
+        new inquirer.Separator("¯\\_(ツ)_/¯"),
+        "Post Bid",
+        new inquirer.Separator("¯\\_(ツ)_/¯"),
+
+
+      ],
+      name: "userPick"
+
+    }
+  ]).then(function (inquiry) {
+
+  })
+
+}
